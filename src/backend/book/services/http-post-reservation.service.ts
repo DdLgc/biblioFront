aee38508a459181378f3e7b4e6652ea6155c1d94
David Lgc <legouellecd@gmail.com>
2023-06-26 14:54:17 +0200

    Fix mishandling

---
+++ src/backend/book/services/http-post-reservation.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,  HttpHeaders} from '@angular/common/http';
+import { Book } from '../book.interface';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpPostReservationService {
+
+  constructor(private httpClient: HttpClient) { }
+  postBooks(id: number): Observable<any> {
+    let returnDateInitial = new Date();
+    returnDateInitial.setDate(returnDateInitial.getDate() + 30);
+    let body = {
+      returnDateInitial: returnDateInitial,
+      loanDate: new Date(),
+      idBook: [
+        "api/books/"+id
+      ],
+      user: "api/users/1"
+    }
+
+    return this.httpClient.post('http://localhost/Biblio-API/public/index.php/api/reservations',body)
+  }
+}
